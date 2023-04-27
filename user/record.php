20464b190e1b4a9d231e34a4eac365a6a09221dc
Tom-Liii <heming7266@gmail.com>
2023-04-27 23:51:37 +0800

    Modify EnterRoom.html

--- user/record.php
+++ user/record.php
@@ -2,12 +2,20 @@
 
 include 'dbconfig.php';
 
-
+// Retrieve the data from the POST request
 $userid = $_POST['userid'];
 $game_record = $_POST['game_record'];
 $start_time = $_POST['start_time'];
 $elapsed_time = $_POST['elapsed_time'];
 $win_status = $_POST['win_status'];
+
+// Escape special characters in the data to prevent SQL injection attacks
+$userid = mysqli_real_escape_string($conn, $userid);
+$game_record = mysqli_real_escape_string($conn, $game_record);
+$start_time = mysqli_real_escape_string($conn, $start_time);
+$elapsed_time = mysqli_real_escape_string($conn, $elapsed_time);
+$win_status = mysqli_real_escape_string($conn, $win_status);
+
 
 echo "userid<br>";
 echo $userid;
