51d2e362a8e4af58b049a7f884743cad47bb0ec3
Tom-Liii <heming7266@gmail.com>
2023-04-29 16:24:54 +0800

    Modify view_goboard.php

--- user/ViewGameRecord.php
+++ user/ViewGameRecord.php
@@ -28,6 +28,7 @@
     </svg>
     <?php
           $userid = $_GET['userid'];
+          $username = $_GET['username'];
           include('dbconfig.php');
           
           session_start();
@@ -41,7 +42,7 @@
           if (mysqli_num_rows($result) > 0) {
               while ($row = mysqli_fetch_assoc($result)) {
                 $_SESSION['game_record'] = $row['game_record'];
-                echo "<tr><td>" . $row['game_id'] . "</td><td>" . $row['userid'] . "</td><td>" . $row['start_time'] . "</td><td>" . $row['elapsed_time'] . "</td><td>" . $row['win_status']  ."</td><td><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-border-all\" viewBox=\"0 0 16 16\" onclick=\"location.href='view_goboard.php?userid=".$row['userid']."';\">
+                echo "<tr><td>" . $row['game_id'] . "</td><td>" . $row['userid'] . "</td><td>" . $row['start_time'] . "</td><td>" . $row['elapsed_time'] . "</td><td>" . $row['win_status']  ."</td><td><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-border-all\" viewBox=\"0 0 16 16\" onclick=\"location.href='view_goboard.php?username=".$username."&userid=".$row['userid']."';\">
                 <path d=\"M0 0h16v16H0V0zm1 1v6.5h6.5V1H1zm7.5 0v6.5H15V1H8.5zM15 8.5H8.5V15H15V8.5zM7.5 15V8.5H1V15h6.5z\"/>
               </svg>";
               }
