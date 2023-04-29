5d1bd79dec92698453ac96a9afad71ab186c0fc0
Tom-Liii <heming7266@gmail.com>
2023-04-29 16:27:55 +0800

    Modify view_goboard.php

--- user/view_goboard.php
+++ user/view_goboard.php
@@ -149,7 +149,13 @@
 
         for (let i = 0; i < 19; i++) {
           for (let j = 0; j < 19; j++) {
-            const value = parseInt(inputString[index]);
+            let value;
+            if (inputString[index] === '-') {
+              value = -parseInt(inputString[index + 1]);
+              index++;
+            } else {
+              value = parseInt(inputString[index]);
+            }
             boardStatus[i][j] = value;
             index++;
           }
@@ -196,7 +202,7 @@ piece.setAttribute("fill", boardStatus[i][j] > 0 ? "black" : "white");
 
 
     </script>
-    <div onclick="redirectToStartPage()">
+    <div onclick="redirectToViewPage()">
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16" style="
     margin-left: 5px;
     margin-top: 5px;
@@ -204,17 +210,17 @@ piece.setAttribute("fill", boardStatus[i][j] > 0 ? "black" : "white");
 ">
   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"></path>
 </svg>
-    
+    Back To View Game Record
   </div>
 
     <script>
-	function redirectToStartPage() {
+	function redirectToViewPage() {
 			      console.log('Redirecting to StartPage...');
 			      var urlParams = new URLSearchParams(window.location.search);
 			      var userid = urlParams.get('userid');
 			      var username = urlParams.get('username');
 			      console.log('userid:', userid);
-			      var newUrl = 'http://34.237.159.19/bigBang/user/StartPage.html?userid=' + userid + '&username=' + username;
+			      var newUrl = 'ViewGameRecord.php?userid=' + userid + '&username=' + username;
 			      console.log('new URL:', newUrl);
 			      window.location.href = newUrl;
 		    }
