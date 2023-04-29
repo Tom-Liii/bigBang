c8f9c43d54d9cc71aec872ee0f4fab48edf39f64
Tom-Liii <heming7266@gmail.com>
2023-04-29 17:11:29 +0800

    Modify AI

--- scripts/aiFunctions.js
+++ scripts/aiFunctions.js
@@ -79,9 +79,8 @@ function easyAI(boardStatus) {
     return bestMove;
   }
   
-
-   function hardAI(boardStatus) {
-        const directions = [
+  function expertAI(boardStatus) {
+    const directions = [
       [0, 1],
       [1, 0],
       [1, 1],
@@ -126,13 +125,13 @@ function easyAI(boardStatus) {
         }
   
         if (count >= 5) {
-          score += 300;
+          score += 10000;
         } else if (count === 4 && empty === 2) {
-          score += 60;
+          score += 1000;
         } else if (count === 3 && empty === 2) {
-          score += 20;
+          score += 100;
         } else if (count === 2 && empty === 2) {
-          score += 5;
+          score += 10;
         } else if (count === 1 && empty === 2) {
           score += 1;
         }
@@ -160,7 +159,7 @@ function easyAI(boardStatus) {
   
     return bestMove;
   }
-
+  
 
 
 
@@ -246,87 +245,3 @@ function easyAI(boardStatus) {
   
     return bestMove;
   }
-
-
-
-  function expertAI(boardStatus, depth = 2) {
-    const directions = [
-      [0, 1],
-      [1, 0],
-      [1, 1],
-      [1, -1]
-    ];
-  
-    const evaluateScore = (x, y, color) => {
-      let score = 0;
-  
-      for (const direction of directions) {
-        let count = 1;
-        let empty = 0;
-  
-        for (let step = 1; step < 5; step++) {
-          const newX = x + step * direction[0];
-          const newY = y + step * direction[1];
-          if (newX >= 0 && newY >= 0 && newX < 19 && newY < 19) {
-            if (boardStatus[newX][newY] === color) {
-              count++;
-            } else if (boardStatus[newX][newY] === 0) {
-              empty++;
-              break;
-            } else {
-              break;
-            }
-          }
-        }
-  
-        for (let step = 1; step < 5; step++) {
-          const newX = x - step * direction[0];
-          const newY = y - step * direction[1];
-          if (newX >= 0 && newY >= 0 && newX < 19 && newY < 19) {
-            if (boardStatus[newX][newY] === color) {
-              count++;
-            } else if (boardStatus[newX][newY] === 0) {
-              empty++;
-              break;
-            } else {
-              break;
-            }
-          }
-        }
-  
-        if (count >= 5) {
-          score += 300;
-        } else if (count === 4 && empty === 2) {
-          score += 60;
-        } else if (count === 3 && empty === 2) {
-          score += 20;
-        } else if (count === 2 && empty === 2) {
-          score += 5;
-        } else if (count === 1 && empty === 2) {
-          score += 1;
-        }
-      }
-  
-      return score;
-    };
-  
-    let bestScore = -Infinity;
-    let bestMove = [0, 0];
-  
-    for (let x = 0; x < 19; x++) {
-      for (let y = 0; y < 19; y++) {
-        if (boardStatus[x][y] === 0) {
-          const currentScore =
-            evaluateScore(x, y, -1) * 1.5 + evaluateScore(x, y, 1);
-  
-          if (currentScore > bestScore) {
-            bestScore = currentScore;
-            bestMove = [x, y];
-          }
-        }
-      }
-    }
-  
-    return bestMove;
-  }
-
